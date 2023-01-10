447dd96b013ad4e8bc56dd399035b3b02625e612
Jamie White <jamie@jgwhite.co.uk>
2023-01-10 13:32:55 +0100

    Struct pointers

--- types/structs/pointers/begin/main.go
+++ types/structs/pointers/begin/main.go
@@ -1,29 +1,34 @@
 // types/structs/pointers/begin/main.go
 package main
 
+import "fmt"
+
 type author struct {
 	first string
 	last  string
 }
 
 // fullName returns the full name of the author
-func (a author) fullName() string {
+func (a *author) fullName() string {
 	return a.first + " " + a.last
 }
 
 // changeName changes the first and last name of the author
-//
+func (a *author) changeName(first, last string) {
+	a.first = first
+	a.last = last
+}
 
 func main() {
-	// a := author{
-	// 	first: "Mar1",
-	// 	last:  "Twain",
-	// }
+	a := author{
+		first: "Mar1",
+		last:  "Twain",
+	}
 
-	// fmt.Println(a.fullName())
+	fmt.Println(a.fullName())
 
 	// call changeName to update name of author
-	//
+	a.changeName("Mark", "Twain")
 
-	// fmt.Println(a.fullName())
+	fmt.Println(a.fullName())
 }
