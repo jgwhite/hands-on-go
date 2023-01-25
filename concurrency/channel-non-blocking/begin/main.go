2161774ecf978bdd519941b7e69b4a13f204c312
Jamie White <jamie@jgwhite.co.uk>
2023-01-25 11:55:56 +0100

    Non-blocking channels

--- concurrency/channel-non-blocking/begin/main.go
+++ concurrency/channel-non-blocking/begin/main.go
@@ -1,8 +1,25 @@
 // concurrency/channel-non-blocking/begin/main.go
 package main
 
-func main() {
-	// declare a signal channel to read from
+import (
+	"fmt"
+	"time"
+)
 
-	// use a default case in select to prevent blocking
+func main() {
+	timeChan1 := time.After(200 * time.Millisecond)
+	timeChan2 := time.After(400 * time.Millisecond)
+
+	for {
+		select {
+		case <-timeChan1:
+			fmt.Println("timeChan1")
+		case <-timeChan2:
+			fmt.Println("timeChan2")
+			return
+		default:
+			fmt.Println("default")
+			time.Sleep(250 * time.Millisecond)
+		}
+	}
 }
